e049cd68c283bf4b3f43a77591b8c9e1f5ae993c
JinGyuGwak <wlsrb2695@naver.com>
2022-09-17 01:41:30 +0900

    챕터7

---
+++ sp5-chap07/src/main/java/config/AppCtx.java
@@ -0,0 +1,24 @@
+package config;
+
+import aspect.ExeTimeAspect;
+import chap07.Calculator;
+import chap07.RecCalculator;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@Configuration
+@EnableAspectJAutoProxy //이 애노테이션을 추가하면 스프링은
+// @Aspect 애노테이션이 붙은 빈 객체를 찾아서 빈 객체의 Pointcut과 Around 사용함
+public class AppCtx {
+	@Bean
+	public ExeTimeAspect exeTimeAspect() {
+		return new ExeTimeAspect();
+	}
+
+	@Bean
+	public Calculator calculator() {
+		return new RecCalculator();
+	}
+
+}
