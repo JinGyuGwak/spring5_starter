5269a51d602346bc62a2f7fcdb850cb8f9aff18f
JinGyuGwak <wlsrb2695@naver.com>
2022-11-23 02:14:14 +0900

    빈생명주기 콜백 함수

--- core/core/src/main/java/hello/core/discount/FixDiscountPolicy.java
+++ core/core/src/main/java/hello/core/discount/FixDiscountPolicy.java
@@ -2,7 +2,10 @@ package hello.core.discount;
 
 import hello.core.member.Grade;
 import hello.core.member.Member;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
 
+@Component
 public class FixDiscountPolicy implements DiscountPolicy{
     private int discountFixAmount=1000;
 
@@ -13,6 +16,5 @@ public class FixDiscountPolicy implements DiscountPolicy{
         } else{
             return 0;
         }
-
     }
 }
