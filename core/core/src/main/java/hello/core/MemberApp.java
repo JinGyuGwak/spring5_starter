140b2237216df260d55d33c66f8a42360d944158
JinGyuGwak <wlsrb2695@naver.com>
2022-11-25 11:07:15 +0900

    테스트

--- core/core/src/main/java/hello/core/MemberApp.java
+++ core/core/src/main/java/hello/core/MemberApp.java
@@ -10,21 +10,17 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class MemberApp {
     public static void main(String[] args) {
 
-
-
         ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
         MemberService memberService = applicationContext.getBean("memberService",MemberService.class);
 
         //        AppConfig appConfig = new AppConfig();
         //MemberService memberService = appConfig.memberService();
 
-
-
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
 
         Member findMember = memberService.findMember(1L);
         System.out.println("newMember = " + member.getName());
-        System.out.println("findMember = " + findMember.getName());
+        System.out.println("find Member = " + findMember.getName());
     }
 }
