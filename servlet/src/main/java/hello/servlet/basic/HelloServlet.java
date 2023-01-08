88e0814cbacaa296773f824eed6c8ce8c34170ab
JinGyuGwak <wlsrb2695@naver.com>
2023-01-08 23:24:26 +0900

    HTTP 서블릿 리퀘스트

---
+++ servlet/src/main/java/hello/servlet/basic/HelloServlet.java
@@ -0,0 +1,23 @@
+package hello.servlet.basic;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "helloServlet", urlPatterns = "/hello")
+public class HelloServlet extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String username = request.getParameter("username");
+        System.out.println("username = " + username);
+
+        response.setContentType("text/plain");
+        response.setCharacterEncoding("utf-8");
+        //위의 response값은 헤더(컨텐트타입)에 들어간다.
+        response.getWriter().write("hello " + username); //write를 사용하면 HTTP 메세지 바디에 데이터가 들어간다
+    }
+
+}
