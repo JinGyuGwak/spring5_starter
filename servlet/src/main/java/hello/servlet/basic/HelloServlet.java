729f29d08d4b96e7d9351192f3caecbb87149534
JinGyuGwak <wlsrb2695@naver.com>
2023-01-15 15:41:34 +0900

    간단한 회원가입 서블릿 구현

--- servlet/src/main/java/hello/servlet/basic/HelloServlet.java
+++ servlet/src/main/java/hello/servlet/basic/HelloServlet.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @WebServlet(name = "helloServlet", urlPatterns = "/hello")
-public class HelloServlet extends HttpServlet {
+public class HelloServlet extends HttpServlet { //이녀석이 서블릿이다
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String username = request.getParameter("username");
