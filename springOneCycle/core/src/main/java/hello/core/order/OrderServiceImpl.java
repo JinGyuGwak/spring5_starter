35c95cc3d4ad4abf5d86f8bf9c7e5b36153a232a
JinGyuGwak <104514223+JinGyuGwak@users.noreply.github.com>
2023-04-01 21:56:32 +0900

    0401 스프링섹션4 공부,

--- springOneCycle/core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ springOneCycle/core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -2,13 +2,19 @@ package hello.core.order;
 
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
 
 public class OrderServiceImpl implements OrderService{
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    private final MemberRepository memberRepository;
+    private final DiscountPolicy discountPolicy;
+
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
 
     @Override
     //주문요청(유저 id,상품명,상품가격) -> 멤버의 등급 조회 -> 할인적용 -> 주문객체생성
